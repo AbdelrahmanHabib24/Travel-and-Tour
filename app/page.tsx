3c0bbc2c595a62e2838b9f427836ab045c73c327
AbdelrahmanHabib24 <your24@email.com>
2025-09-11 13:20:41 +0300

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,26 @@ import dynamic from "next/dynamic";
 import ProtectedHome from "./component/Home";
 import AppRoot from "./component/AppRoot";
 import Link from "next/link";
-import PageLoader from "@/app/component/PageLoader"; // ✅
+import PageLoader from "@/app/component/PageLoader"; 
 
 const Hero = dynamic(() => import("./component/Hero"), {
   ssr: true,
-  loading: () => <PageLoader />, // ⬅️
+  loading: () => <PageLoader />, 
 });
 
 const Feature = dynamic(() => import("./component/Feature"), {
   ssr: false,
-  loading: () => <PageLoader />, // ⬅️
+  loading: () => <PageLoader />, 
 });
 
 const Listing = dynamic(() => import("./component/Listing"), {
   ssr: false,
-  loading: () => <PageLoader />, // ⬅️
+  loading: () => <PageLoader />, 
 });
 
 const Testimonials = dynamic(() => import("./component/Testimonials"), {
   ssr: false,
-  loading: () => <PageLoader />, // ⬅️
+  loading: () => <PageLoader />, 
 });
 
 export default function Home() {
