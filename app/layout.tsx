88ed9081c57eac90458f24d5103f7c00c2ae0e35
AbdelrahmanHabib24 <your24@email.com>
2025-09-12 21:01:00 +0300

    Fixed Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Header from "@/app/component/Header";
 import Footer from "@/app/component/Footer";
 import Providers from "./providers";
+import { Suspense } from "react";
+import GlobalLoading from "./loading";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -17,20 +19,29 @@ export const metadata: Metadata = {
   description: "Tour and travel app",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
-        <link rel="preload" as="video" href="/video.mp4" type="video/mp4" />
+        <link rel="preload" as="video" href="/Video.mp4" type="video/mp4" />
       </head>
-      <body className={`bg-primary text-tertiary transition-all duration-300 ${nunito.variable}`}>
+      <body
+        className={`bg-primary text-tertiary transition-all duration-300 ${nunito.variable} `}
+      >
         <Providers>
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <Footer />
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <Suspense fallback={<GlobalLoading />}>
+              <main className="flex-1">{children}</main>
+            </Suspense>
+            <Footer />
+          </div>
         </Providers>
       </body>
     </html>
   );
 }
-
