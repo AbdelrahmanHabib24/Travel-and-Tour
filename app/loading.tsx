dac8578cabb92198cc810f4af083978cd89fc7c5
AbdelrahmanHabib24 <your24@email.com>
2025-10-09 11:15:00 +0300

    enhance loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,21 @@
 // app/loading.tsx
 "use client";
 
-import Lottie from "lottie-react";
 import loadingAnimation from "@/app/loading.json";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export default function GlobalLoading() {
   return (
     <div className="fixed inset-0 z-[9999] flex items-center justify-center">
-  <div className="absolute inset-0 bg-black/20 backdrop-blur-md"></div>
-  <Lottie animationData={loadingAnimation} loop autoplay className="w-40 h-40" />
-</div>
-
+      <div className="absolute inset-0 bg-white"></div>
+      <Lottie
+        animationData={loadingAnimation}
+        loop
+        autoplay
+        className="w-40 h-40"
+      />
+    </div>
   );
 }
