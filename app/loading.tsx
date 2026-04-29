88e64c3629e1c42a694419ba06c63f8a4aab5396
AbdelrahmanHabib24 <abdelrahmanhabib502@gmail.com>
2026-04-29 18:58:17 +0300

    feat: implement global loading component using Lottie animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,22 +1,16 @@
 // app/loading.tsx
 "use client";
 
-import dynamic from "next/dynamic";
-
-const DotLottiePlayer = dynamic(
-  () => import('@dotlottie/react-player').then(mod => mod.DotLottiePlayer),
-  { ssr: false }
-);
+import Lottie from "lottie-react";
+import loadingAnimation from "./loading.json";
 
 export default function GlobalLoading() {
   return (
-    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white/95 backdrop-blur-xl px-4">
-      {/* Responsive container: scales with screen size but has max limits to stay professional */}
+    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white px-4 transition-all duration-300">
       <div className="w-[60vw] max-w-[160px] md:max-w-[240px] lg:max-w-[300px] aspect-square flex items-center justify-center -mt-10 md:-mt-16">
-        <DotLottiePlayer
-          src="/loading.lottie"
-          autoplay
-          loop
+        <Lottie 
+          animationData={loadingAnimation} 
+          loop={true} 
           className="w-full h-full object-contain"
         />
       </div>
