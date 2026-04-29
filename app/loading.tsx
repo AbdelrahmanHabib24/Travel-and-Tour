91805fd1c5c3caa5774a3e9a3b97e9b9d205e365
AbdelrahmanHabib24 <abdelrahmanhabib502@gmail.com>
2026-04-29 19:21:19 +0300

    feat: add global loading component with animated GIF support

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,16 +1,13 @@
 // app/loading.tsx
-"use client";
-
-import Lottie from "lottie-react";
-import loadingAnimation from "./loading.json";
+import React from "react";
 
 export default function GlobalLoading() {
   return (
-    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white px-4 transition-all duration-300">
+    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white px-4">
       <div className="w-[60vw] max-w-[160px] md:max-w-[240px] lg:max-w-[300px] aspect-square flex items-center justify-center -mt-10 md:-mt-16">
-        <Lottie 
-          animationData={loadingAnimation} 
-          loop={true} 
+        <img 
+          src="/loading_20260429161850.gif" 
+          alt="Loading..." 
           className="w-full h-full object-contain"
         />
       </div>
